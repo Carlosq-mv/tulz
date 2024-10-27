7f52067b4bf7be6e18276458cae5770893fbd896
Carlos Quiroz <cquiroz7869@gmail.com>
2024-10-26 19:26:21 -0700

    added middleware

--- server/main.py
+++ server/main.py
@@ -6,16 +6,27 @@ import os
 
 load_dotenv()
 
+from routers.user_routes import u_routes
+from database import Base, engine
+from routers.middleware.auth import JWTMiddleWare
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
-allowed_url = os.getenv('ALLOWED_URL')
+allowed_url = os.getenv("ALLOWED_URL")
 if not allowed_url:
     raise ValueError('ALLOWED_URL env variable not set')
 
+allowed_origins = [allowed_url]  # Wrap the URL in a list
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_url,
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+app.add_middleware(JWTMiddleWare)
+app.include_router(u_routes, prefix="/user", tags=["Users"])
