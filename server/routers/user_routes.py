c7ed05a011627c7e9aa5bee908b6a14a0a73345b
Carlos Quiroz <cquiroz7869@gmail.com>
2024-11-11 16:21:25 -0800

    updated user routes w/ new dependecies

--- server/routers/user_routes.py
+++ server/routers/user_routes.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import Response
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
 from datetime import timedelta
 
-from dependencies import get_db
-from schemas.user_schema import UserResponse, UserCreate, UserBase, UserLogin
-from schemas.token_schema import TokenData
+from dependencies import get_user_services
+from schemas.user_schema import UserResponse, UserCreate, UserLogin
 from models.user import User
 from routers.middleware.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MIN
+from actions.services.userServices import UserServices
 
 
 u_routes = APIRouter()
@@ -16,101 +16,43 @@ u_routes = APIRouter()
 
 # create account API route
 @u_routes.post("/create-account", response_model=UserResponse)
-def create_account(user: UserCreate, db: Session = Depends(get_db)) -> User:
-    # check if all fields are not empty
-    if not user.username or not user.email or not user.password or not user.name:
-        raise HTTPException(status_code=400, detail="All fields are required. Please try again.")
-
-    if "@" not in user.email:
-        raise HTTPException(status_code=400, detail="Invalid email format.Please try again.")
-    # check if the user inputted email or username already exists
-    existing_user = db.query(User).filter(
-        or_ (
-            User.email == user.email,
-            User.username == user.username
-        )
-    ).first()
-
-    # if it does exists throw HTTP error
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email and(or) username already registered.")
-    return create_user(db, user) 
+async def create_account(user: UserCreate, user_services: UserServices = Depends(get_user_services)) -> User:
+    created_user = await user_services.add_user(user)
+    return created_user
 
 
 # login API route
 @u_routes.post("/login", response_model=UserResponse)
-def login(user: UserLogin, response: Response, db: Session = Depends(get_db)) -> User:
-# check if all fields are not empty
-    if not user.username or not user.email or not user.password:
-        raise HTTPException(status_code=400, detail="All fields are required. Please try again.")
+async def login(user: UserLogin, response: Response, user_services: UserServices = Depends(get_user_services)) -> User:
 
-    user_ = db.query(User).filter(User.email == user.email, User.username == user.username).first()
-    if user_ is None:
-        raise HTTPException(status_code=404, detail="User does exists")
-    # check if password is correct
-    if not user_.check_password(user.password):
-       raise HTTPException(status_code=404, detail="The password you entered is incorrect. Please try again.") 
- 
-    # set jwt token in cookie
     expires_time = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MIN)
-    jwt_token = create_access_token({"username":user_.username, "email":user_.email}, expires_delta=expires_time)
+    jwt_token = create_access_token({"username":user.username, "email":user.email}, expires_delta=expires_time)
     response.set_cookie(key="jwt_token", value=jwt_token, httponly=True, samesite="None", secure=True) 
-    user_.is_logged_in = True
-    db.commit()
-    
-    return user_
+
+    return await user_services.login_user(user)
 
 
-# logout route
+# # logout route
 @u_routes.post("/logout", response_model=UserResponse)
-def logout(response: Response, request: Request, db: Session = Depends(get_db)):
+async def logout(response: Response, request: Request, user_services: UserServices = Depends(get_user_services)):
     username =  request.state.user.get("username")
     email = request.state.user.get("email")
-    current_user = db.query(User).filter(User.username == username, User.email == email).first()
-    current_user.is_logged_in = False
+
     response.delete_cookie("jwt_token")
-    return current_user
+    return await user_services.logout_user(username, email)
 
 
-# get current auth user API route
+# # get current auth user API route
 @u_routes.get("/current-user", response_model=UserResponse) 
-def current_user(request: Request, db: Session = Depends(get_db)):
+async def current_user(request: Request, user_services: UserServices = Depends(get_user_services)):
     username =  request.state.user.get("username")
     email = request.state.user.get("email")
-    user = db.query(User).filter(User.username == username, User.email == email).first()
-    return user
+
+    return await user_services.get_current_user(username, email)
 
 
-# get token info
-@u_routes.get("/token-data", response_model=TokenData)
-def token_data(request: Request):
-    return request.state.token
-
-
-
-
-
-
-
-
-
-
-# get a particular user by id
-@u_routes.get("/{id}", response_model=UserResponse)
-def get_user(id: int, db: Session = Depends(get_db)) -> User:
-    user = db.query(User).filter(User.id == id).first()
-    return user
-
-# get all of the users
-@u_routes.get("/all-users") 
-def get_all_users(db: Session = Depends(get_db)) -> list[UserResponse]:
-    users = db.query(User).all()
-    return [UserResponse.model_validate(u) for u in users]
-
-# helper methods to create a new user
-def create_user(db: Session, user: UserCreate) -> User:
-    new_user = User(name=user.name, email=user.email, username=user.username)
-    new_user.set_password(user.password)
-    db.add(new_user)
-    db.commit()
-    return new_user
+# # get all of the users
+# @u_routes.get("/all-users",response_model=list[UserResponse]) 
+# def get_all_users(db: Session = Depends(get_db)) -> list[UserResponse]:
+#     users = db.query(User).all()
+#     return [UserResponse.model_validate(u) for u in users]
