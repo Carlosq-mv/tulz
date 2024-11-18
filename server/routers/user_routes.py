81ea9c44721eb119bd9c4d8f226f49ae46d0990c
Carlos Quiroz <cquiroz7869@gmail.com>
2024-11-17 18:10:53 -0800

    refactor: current-user api uses request state instead of db

--- server/routers/user_routes.py
+++ server/routers/user_routes.py
@@ -5,7 +5,8 @@ from datetime import timedelta
 from dependencies import get_user_services
 from schemas.user_schema import UserResponse, UserCreate, UserLogin
 from models.user import User
-from routers.middleware.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MIN
+from routers.middleware.auth import create_access_token 
+from actions.util.jwtHelper import ACCESS_TOKEN_EXPIRE_MIN
 from actions.services.userServices import UserServices
 
 
@@ -33,20 +34,16 @@ async def login(user: UserLogin, response: Response, user_services: UserServices
 # # logout route
 @u_routes.post("/logout", response_model=UserResponse)
 async def logout(response: Response, request: Request, user_services: UserServices = Depends(get_user_services)):
-    username =  request.state.user.get("username")
-    email = request.state.user.get("email")
+    user = request.state.user
 
     response.delete_cookie("jwt_token")
-    return await user_services.logout_user(username, email)
+    return await user_services.logout_user(user.username, user.email)
 
 
 # # get current auth user API route
 @u_routes.get("/current-user", response_model=UserResponse) 
-async def current_user(request: Request, user_services: UserServices = Depends(get_user_services)):
-    username =  request.state.user.get("username")
-    email = request.state.user.get("email")
-
-    return await user_services.get_current_user(username, email)
+async def current_user(request: Request):
+    return request.state.user
 
 
 # # get all of the users
