2314afb8c19660aa6671f03644f20bfc92a8004e
Carlos Quiroz <cquiroz7869@gmail.com>
2024-11-18 11:34:21 -0800

    changed routers to api (name change)

--- server/main.py
+++ server/main.py
@@ -6,9 +6,9 @@ import os
 
 load_dotenv()
 
-from routers.user_routes import u_routes
+from routers.user_api import u_api
 from routers.websockets import wb_routes
-from routers.friendship_api import f_routes
+from routers.friendship_api import f_api
 from database import Base, engine
 from routers.middleware.auth import JWTMiddleWare
 
@@ -31,6 +31,6 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-app.include_router(u_routes, prefix="/user", tags=["Users"])
+app.include_router(u_api, prefix="/user", tags=["Users"])
 app.include_router(wb_routes, tags=["Websocket"])
-app.include_router(f_routes, prefix="/friend", tags=["Friends"])
+app.include_router(f_api, prefix="/friend", tags=["Friends"])
