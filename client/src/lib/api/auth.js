a8e022f1f46e3d7d995dadada7a90baf891866fb
carlosq-mv <cquiroz7869@gmail.com>
2024-12-14 13:11:25 -0800

    sign up fetch api

--- client/src/lib/api/auth.js
+++ client/src/lib/api/auth.js
@@ -11,15 +11,39 @@ export async function login(loginData) {
 
     // check for non-200 status codes
     if (!res.ok) {
-      const errorData = await res.json();
-      throw new Error(errorData.detail || "Failed to login up.");
+      const errorData = await res.json()
+      throw new Error(errorData.detail || "Failed to login up.")
     }
 
     // parse and return the response JSON
-    return await res.json();
+    return await res.json()
 
   } catch (error) {
     throw error;
   }
+}
 
+export async function signup(sigupData) {
+  try {
+    const res = await fetch("http://localhost:8000/user/create-account", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(sigupData)
+    })
+
+    // check for non-200 status codes
+    if (!res.ok) {
+      const errorData = await res.json()
+      throw new Error(errorData.detail || "Failed to sign up.")
+    }
+
+    // parse and return the response JSON
+    return await res.json()
+
+  } catch (error) {
+    throw error
+  }
 }
