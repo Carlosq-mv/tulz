ca8a9dbe5ffbc8247d8a1997b478b27ed63ad657
carlosq-mv <cquiroz7869@gmail.com>
2024-12-13 21:53:52 -0800

    formfield component

---
+++ client/src/lib/components/FormField.svelte
@@ -0,0 +1,21 @@
+<script>
+	let { type, labelname, placeholder, id, value = $bindable(), children } = $props();
+</script>
+
+<div class="form-control mt-4">
+	<label for={id} class="label">
+		<span class="label-text">{labelname}</span>
+	</label>
+	<label class="input input-bordered flex items-center gap-2">
+		{#if children}
+			{@render children()}
+		{/if}
+		<input class="grow" bind:value={value} {type} {placeholder} />
+	</label>
+</div>
+
+<!-- let form = $state({
+  username: "", 
+  email: "",
+  password: ""
+}) -->
