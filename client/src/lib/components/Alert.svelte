cdbfe30728afaec8315f6d1790de08cde2af74f7
carlosq-mv <cquiroz7869@gmail.com>
2024-12-19 13:59:23 -0800

    ui tweaks

--- client/src/lib/components/Alert.svelte
+++ client/src/lib/components/Alert.svelte
@@ -2,10 +2,10 @@
   let { errorMessage } = $props();
 </script>
 
-<div role="alert" class="alert alert-error mt-6 flex h-12">
+<div role="alert" class="alert alert-error mt-6 flex h-16">
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-5 w-5 shrink-0 stroke-current"
+    class="h-8 w-6 shrink-0 stroke-current"
     fill="none"
     viewBox="0 0 24 24"
   >
@@ -16,5 +16,5 @@
       d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
     />
   </svg>
-  <span>{errorMessage || 'An unexpected error occurred.'}</span>
+  <span class="text-center font-semibold">{errorMessage || 'An unexpected error occurred.'}</span>
 </div>
