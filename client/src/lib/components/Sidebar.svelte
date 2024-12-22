8b3e71e7a845cfb93bd1d5002ec775cafcc77978
carlosq-mv <cquiroz7869@gmail.com>
2024-12-21 21:13:36 -0800

    nav and sidebar components

---
+++ client/src/lib/components/Sidebar.svelte
@@ -0,0 +1,60 @@
+<script>
+  let { children } = $props();
+</script>
+
+<div class="drawer lg:drawer-open">
+  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col items-center justify-center">
+    <!-- Page content here -->
+    {#if children}
+      {@render children()}
+    {/if}
+    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden"> Open drawer </label>
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="menu bg-base-200 text-base-content flex h-screen w-64 flex-col p-4">
+      <!-- Sidebar content here -->
+      <li><a href="/">Sidebar Item 1</a></li>
+      <li><a href="/">Sidebar Item 2</a></li>
+      <div class="flex-grow"></div>
+
+      <!-- Theme switcher -->
+      <div class="divider"></div>
+      <div class="mb-24 flex items-center justify-center">
+        <label class="flex cursor-pointer gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <circle cx="12" cy="12" r="5" />
+            <path
+              d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
+            />
+          </svg>
+          <input type="checkbox" value="garden" class="toggle theme-controller scale-75" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        </label>
+      </div>
+    </ul>
+  </div>
+</div>
