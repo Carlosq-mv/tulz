0e1f03d0a1b2ecde2f5681be457424e977620164
carlosq-mv <cquiroz7869@gmail.com>
2024-12-24 22:42:57 -0800

    refactor alert component

--- client/src/lib/components/Alert.svelte
+++ client/src/lib/components/Alert.svelte
@@ -1,20 +1,10 @@
 <script>
-  let { errorMessage } = $props();
+  let { message, type } = $props();
 </script>
 
-<div role="alert" class="alert alert-error mt-6 flex h-16">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="h-8 w-6 shrink-0 stroke-current"
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-    />
-  </svg>
-  <span class="text-center font-semibold">{errorMessage || 'An unexpected error occurred.'}</span>
+<div role="alert" class={`alert mt-6 ${type?.style}`}>
+  <div class="flex h-6 w-6 shrink-0 items-center stroke-current">
+    <img src={type?.icon} alt="icon" />
+  </div>
+  <span class="text-center">{message}</span>
 </div>
---
+++ client/src/lib/constants/enums.js
@@ -0,0 +1,10 @@
+export const AlertType = {
+  ERROR: {
+    style: 'alert-error',
+    icon: 'icons/error-icon.svg'
+  }, 
+  SUCCESS: {
+    style: 'alert-success',
+    icon: 'icons/success-icon.svg'
+  }
+}
