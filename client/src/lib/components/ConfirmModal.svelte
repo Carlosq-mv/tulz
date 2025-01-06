a0d34af907e6106d0cc4be18e65b538ba09d292d
carlosq-mv <cquiroz7869@gmail.com>
2025-01-05 16:02:01 -0800

    modal

---
+++ client/src/lib/components/ConfirmModal.svelte
@@ -0,0 +1,16 @@
+<script>
+  let { id, title, message, buttonName, onConfirm } = $props()
+</script>
+
+<dialog id={id} class="modal">
+  <div class="modal-box">
+    <h3 class="text-lg font-bold">{title}</h3>
+    <p class="py-4">{message}</p>
+    <div class="modal-action">
+      <form method="dialog" class="flex gap-8">
+        <button class="btn btn-sm btn-error">Cancel</button>
+        <button class="btn btn-sm btn-warning" onclick={onConfirm}>{buttonName}</button>
+      </form>
+    </div>
+  </div>
+</dialog>
