e6e6954c46baafe84b3304195b5ac9df8cbc8c61
carlosq-mv <cquiroz7869@gmail.com>
2025-01-05 16:25:59 -0800

    idk this is a mess fix later

--- server/actions/dal/contactDAO.py
+++ server/actions/dal/contactDAO.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from sqlalchemy import exists, not_
-from sqlalchemy.orm import Session
+from sqlalchemy import and_, exists, not_, or_
+from sqlalchemy.orm import Session, joinedload
 from sqlalchemy.exc import SQLAlchemyError
 
 from schemas.user_schema import UserBase, UserResponse
@@ -22,10 +22,15 @@ class ContactDAO():
 
 
     # create a new contact request
-    def request_to_add_contact(self, user: User, contact: ContactCreate) -> Contact:
+    def create_contact(self, c: ContactForm) -> Contact:
         try:
-            # contact with REQUESTED status
-            new_contact = Contact(user_id=user.id, username=contact.username, status=Status.REQUESTED)
+            # create new contact with REQUESTED status
+            new_contact = Contact(
+                current_user_id=c.current_user_id,
+                friend_id=c.friend_id,
+                status=Status.REQUESTED,
+                last_updated=datetime.now()
+            )
             self.db.add(new_contact)
             self.db.commit()
 
@@ -38,10 +43,31 @@ class ContactDAO():
     
    
     # update the contact status (e.g. from REQUESTED -> ACCEPTED) 
-    def update_contact_status(self, user: User, contact_id: int, status: Status) -> Contact:
+    def update_contact_status(self, identifier: ContactForm | int, status: Status, user: User | None = None) -> Contact:
         try:
-            # retrieve the contact
-            contact = self.db.query(Contact).filter(Contact.user_id == user.id, Contact.id == contact_id).first()
+            # Determine if the identifier is a ContactForm or an ID
+            if isinstance(identifier, ContactForm):
+                # Query based on current_user_id and friend_id
+                contact = (
+                    self.db.query(Contact)
+                    .filter(
+                        Contact.current_user_id == identifier.current_user_id,
+                        Contact.friend_id == identifier.friend_id,
+                    )
+                    .first()
+                )
+            elif isinstance(identifier, int):
+                # Query based on the contact ID
+                contact = self.db.query(Contact).filter(
+                    and_(
+                       or_(
+                           Contact.current_user_id == user.id, 
+                           Contact.friend_id == user.id, 
+                       ),
+                       Contact.id == identifier
+                    )
+                ).first()
+
             
             if not contact:
                 raise ValueError("Contact not found.")
@@ -57,7 +83,7 @@ class ContactDAO():
             contact.status = status
             self.db.commit()
 
-            # return contact objects
+            # return contact object
             return contact
         except SQLAlchemyError as e:
             # handle database errors
@@ -67,34 +93,113 @@ class ContactDAO():
 
     # get all of the current user's contacts, regardless of status
     def get_all_contacts(self, user: User) -> list[Contact]:
-        return self.db.query(Contact).filter(Contact.user_id == user.id).all()
+        return self.db.query(Contact).filter(
+            Contact.current_user_id == user.id
+        ).all()
+
+    # TODO: temp sol'n 
+    # get contacts by status
+    def get_contacts(self, user: User, status: Status):
+        data = (self.db.query(Contact)
+                    .join(User, or_(Contact.current_user_id == User.id, Contact.friend_id == User.id))
+                    .filter(
+                        and_ (
+                            or_ (
+                                Contact.current_user_id == User.id, 
+                                Contact.friend_id == User.id 
+                            ),
+                            Contact.status == status,
+                        )
+                    )
+                    .options(
+                        joinedload(Contact.user_1).defer(User.password),
+                        joinedload(Contact.user_2).defer(User.password),
+                             )
+                    .all()
+                )
+        result = []
+        for contact in data:
+            # determine which user is not the current user and assign as the 'other user'
+            if contact.current_user_id == user.id:
+                contact.other_user = contact.user_2
+            else:
+                contact.other_user = contact.user_1
+
+            # add the contact j
+            result.append(contact)
+        
+        return result
    
-   
+    # get all the request sent by the current user 
+    def get_requests_sent(self, user: User):
+        return (self.db.query(Contact)
+                    .filter(Contact.current_user_id == user.id, Contact.status == Status.REQUESTED)
+                    .options(
+                        joinedload(Contact.user_2).defer(User.password)
+                    )
+                    .all()
+                )
+        
+
     # get contact via search criteria (contact id, or contact username) 
-    def get_contact(self, contact: ContactSearch) -> Contact:
-        if contact.id :
-            return self.db.query(Contact).filter(Contact.id == contact.id).first()
-        if contact.username:
-            return self.db.query(Contact).filter(Contact.username == contact.username).first()
+    def search_contact(self, criteria: ContactSearch) -> Contact:
+        if criteria.id :
+            return self.db.query(Contact).filter(Contact.id == criteria.id).first()
+        if criteria.username:
+            # perform a join
+            return (self.db.query(Contact, User.username)
+                .join(User, or_(
+                    User.id == Contact.requester_id,  # Join on requester
+                    User.id == Contact.receiver_id     # Or join on receiver
+                ))
+                .filter(User.username == criteria.username)
+                .first()
+            )
+            # return self.db.query(Contact).filter(Contact.username == criteria.username).first()
 
 
-    # check if contact exists by username
-    def contact_exists(self, user: User, contact: ContactResponse) -> bool:
+    def get_contact_requests_to_me(self, user: User):
+        return (self.db.query(Contact)
+                    .join(User, Contact.friend_id == User.id)   # Join on the friend user (receiver of the request)
+                    .filter(
+                        Contact.friend_id == user.id,           # Exclude self-contacts (if applicable)
+                        Contact.status == Status.REQUESTED      # Filter by Requested status
+                    )
+                    .options(joinedload(Contact.user_1))     # Load the user details of the foreign user
+                    .all()
+                )
+       
+        
+    # check if contact exists 
+    def contact_exists(self, c: ContactForm) -> bool:
         return self.db.query(
             exists().where(
-                Contact.user_id == user.id, 
-                Contact.username == contact.username,
-                not_(Contact.status.in_(self.excluded_status))
+                and_(
+                   or_(
+                        Contact.current_user_id == c.current_user_id, 
+                        Contact.friend_id == c.current_user_id,
+                    ), 
+                    or_(
+                        Contact.friend_id == c.friend_id,
+                        Contact.current_user_id == c.friend_id,
+                    ),
+                    not_(Contact.status.in_(self.excluded_status))
+                )
             )
         ).scalar()
 
-
-    # check if contact exists by id
     def contact_exists_by_id(self, user: User, contact_id: int) -> bool:
-        return self.db.query(
+        res = self.db.query(
             exists().where(
-                Contact.user_id == user.id, 
-                Contact.id == contact_id,
-                not_(Contact.status.in_(self.excluded_status))
+                and_(
+                    or_(
+                        Contact.current_user_id == user.id, 
+                        Contact.friend_id == user.id
+                    ),
+                    Contact.id == contact_id,
+                    not_(Contact.status.in_(self.excluded_status))
+                )
             )
         ).scalar()
+        print(res, contact_id, user)
+        return res
--- server/actions/services/contactService.py
+++ server/actions/services/contactService.py
@@ -13,89 +13,94 @@ class ContactService():
         self.user_dao = user_dao
 
 
-    async def add_contact(self, user: User, contact: ContactCreate) -> ContactResponse:
-        await self.has_empty_fields(contact)
-        await self.self_contact(user, contact)
-        await self.contact_exists(user, contact)
-        await self.user_contact_exists(contact.username)
-        
-        return self.dao.request_to_add_contact(user, contact)
+    async def add_contact(self, contact: ContactForm) -> ContactResponse:
+        await self.has_empty_fields(contact)    # check if the payload is present
+        await self.is_self_contact(contact)   # check current user isn't adding themself as contact
+        await self.contact_exists(contact)    # check if the contact already exists
+        await self.does_user_exists(contact)        # check if the user to be added as contact, exists
+        # add/create the new contact between the current user, and the recipient
+        return self.dao.create_contact(contact)
 
 
     async def remove_contact(self, user: User, id: int) -> ContactResponse:
         await self.contact_exists_by_id(user, id)
-        return self.dao.update_contact_status(user, id, Status.REMOVED) 
+        return self.dao.update_contact_status(id, Status.REMOVED, user)
+
    
-    
     async def block_contact(self, user: User, id: int) -> ContactResponse:
         await self.contact_exists_by_id(user, id)
-        return self.dao.update_contact_status(user, id, Status.BLOCKED)
+        return self.dao.update_contact_status(id, Status.BLOCKED, user)
 
 
     async def accept_contact(self, user: User, id: int) -> ContactResponse:
         await self.contact_exists_by_id(user, id)
-        return self.dao.update_contact_status(user, id, Status.ACCEPTED)
+        return self.dao.update_contact_status(id, Status.ACCEPTED, user)
 
     
     async def reject_contact(self, user: User, id: int) -> ContactResponse:
         await self.contact_exists_by_id(user, id)
-        return self.dao.update_contact_status(user, id, Status.REJECTED)
+        return self.dao.update_contact_status(id, Status.REJECTED, user)
 
 
     async def unblock_contact(self, user: User, id: int) -> ContactResponse:
         await self.contact_exists_by_id(user, id)
-        return self.dao.update_contact_status(user, id, Status.PENDING)
+        return self.dao.update_contact_status(id, Status.PENDING, user)
     
 
     async def get_all_contacts(self, user: User) -> list[ContactResponse]:
         return self.dao.get_all_contacts(user)
     
-    #TODO: finish these service methods & the dao methods associated with this
-    async def get_blocked_contacts():
-        pass
-
-    async def get_my_contacts():
-        pass
-
-    async def get_requests_for_contact():
-        pass
+    
+    async def get_contact_requests_to_me(self, user: User):
+        return self.dao.get_contact_requests_to_me(user)
+    
+    async def get_blocked_contacts(self, user: User):
+        return self.dao.get_contacts(user, Status.BLOCKED)
 
 
+    async def get_my_contacts(self, user: User):
+        return self.dao.get_contacts(user, Status.ACCEPTED)
 
-    async def get_contact(self, contact_criteria: ContactSearch) -> ContactResponse:
+
+    async def get_requests_sent(self, user: User):
+        return self.dao.get_requests_sent(user)
+
+
+    async def search_contact(self, contact_criteria: ContactSearch) -> ContactResponse:
         if contact_criteria is None:
             raise HTTPException(status_code=400, detail="No search criteria present.")
 
         if contact_criteria.id is None and contact_criteria.user_id is None and contact_criteria.username is None:
             raise HTTPException(status_code=400, detail="No search criteria present.")
 
-        contact = self.dao.get_contact(contact_criteria)
+        contact = self.dao.search_contact(contact_criteria)
         if contact is None:
             raise HTTPException(status_code=400, detail="Contact does not exist.")
         
         return contact
 
 
-    async def user_contact_exists(self, username: str) -> None:
-        if self.user_dao.get_user_by_username(username) is None:
+    async def does_user_exists(self, c: ContactForm) -> None:
+        if self.user_dao.get_user_by_id(c.friend_id) is None:
             raise HTTPException(status_code=400, detail="User does not exists.")
 
 
     async def contact_exists_by_id(self, user: User, contact_id: int) -> None:
         if not self.dao.contact_exists_by_id(user, contact_id):
-            raise HTTPException(status_code=400, detail="Can not delete this contact.")    
+            raise HTTPException(status_code=400, detail="Contact does not exist.")    
     
-    async def contact_exists(self, user: User, contact: ContactResponse) -> None:
-        if self.dao.contact_exists(user, contact):
+    async def contact_exists(self, c: ContactResponse) -> None:
+        print(self.dao.contact_exists(c))
+        if self.dao.contact_exists(c):
              raise HTTPException(status_code=400, detail="User is already in your contact list.")
 
 
-    async def has_empty_fields(self, contact: ContactCreate) -> None:
-        if not contact.username:
-            raise HTTPException(status_code=400, detail="Username needed to add contact.")
+    async def has_empty_fields(self, c: ContactForm) -> None:
+        if not c.current_user_id or not c.friend_id:
+            raise HTTPException(status_code=400, detail="Fill required fields to add contact.")
     
 
-    async def self_contact(self, user: User, contact: ContactCreate) -> None:
-        if user.username == contact.username:
+    async def is_self_contact(self, c: ContactForm) -> None:
+        if c.current_user_id == c.friend_id:
             raise HTTPException(status_code=400, detail="Can not add yourself as a contact.")
     
